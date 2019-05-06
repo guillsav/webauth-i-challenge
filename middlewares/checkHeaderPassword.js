d6082b081dbc5fd19d7270b2c1f40fff6ae62537
guillsav <guillsav@gmail.com>
2019-05-06 16:28:32 -0400

    mvp done

---
+++ middlewares/checkHeaderPassword.js
@@ -0,0 +1,23 @@
+const bcrypt = require('bcrypt');
+const db = require('../users/users-model.js');
+
+function checkHeaderPassword(req, res, next) {
+  const {username, password} = req.headers;
+  const founduser = db
+    .getUser(username)
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        next();
+      } else {
+        res.status(401).json({errorMessage: 'Invalid Credentials!'});
+      }
+    })
+    .catch(err =>
+      res.status(500).json({
+        error: err,
+        errorMessage: 'Error while checking for the users credentials.'
+      })
+    );
+}
+
+module.exports = checkHeaderPassword;
