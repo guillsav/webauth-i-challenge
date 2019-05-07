62f3a2c485b332275d621016811051049b793fcd
guillsav <guillsav@gmail.com>
2019-05-07 15:02:40 -0400

    added Joi for validation, Cookies and session is working

--- api/server.js
+++ api/server.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const session = require('express-session');
 const usersRouter = require('../users/users-router.js');
-const registerRouter = require('../auths/register-router.js');
-const loginRouter = require('../auths/login-router.js');
+const authsRouter = require('../auths/auths-router.js');
 
 const server = express();
 
+const sessionConfig = {
+  name: 'bearer',
+  secret: 'wIjoxNTUG9lIiwiYW2LThmMGEtNIzYyIsImlh',
+  cookie: {
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60 * 1,
+    secure: false
+  },
+  resave: false,
+  saveUninitialized: true
+};
+
+server.use(session(sessionConfig));
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
 // Routers
-server.use('/api/register', registerRouter);
-server.use('/api/login', loginRouter);
+server.use('/api/auth', authsRouter);
 server.use('/api/users', usersRouter);
 
 module.exports = server;
